67d7fc62e9528d014818fa1a4dce3e601e729991
firdavsurunov <urunovfirdavsjon@gmail.com>
2024-11-15 11:55:55 +0500

    fix(prod)

--- src/features/courses/components/create-course/services.ts
+++ src/features/courses/components/create-course/services.ts
@@ -11,7 +11,7 @@ export const useCreateCourse = () => {
         form.image = form.image[0];
       }
       const fd = new FormData();
-      Object.entries(form).forEach(([key, value]) => fd.append(key, value));
+      Object.entries(form).forEach(([key, value]) => fd.append(key, value instanceof File ? value : String(value)));
       return apiService
         .post("/course/create", fd, {
           headers: {
